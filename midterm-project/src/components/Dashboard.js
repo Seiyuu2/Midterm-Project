f04cab7ec2f29e5041504a6529ebc4437b5a7c31
Seiyuu2 <kurtlylep@gmail.com>
2024-10-14 19:09:52 +0800

    Full Design Rework

--- midterm-project/src/components/Dashboard.js
+++ midterm-project/src/components/Dashboard.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './styles/DashboardStyles.css';
 
 const Dashboard = () => {
   const navigate = useNavigate();
 
   return (
     <div className="dashboard-container">
-      <h1>Inventory Management Dashboard</h1>
-      <div className="buttons-container">
-        <button onClick={() => navigate('/add-item')}>Add Item</button>
-        <button onClick={() => navigate('/update-item')}>Update Item</button>
-        <button onClick={() => navigate('/remove-item')}>Remove Item</button>
-        <button onClick={() => navigate('/display-items-category')}>Display Items by Category</button>
-        <button onClick={() => navigate('/display-all-items')}>Display All Items</button>
-        <button onClick={() => navigate('/search-item')}>Search Item</button>
-        <button onClick={() => navigate('/sort-items')}>Sort Items</button>
-        <button onClick={() => navigate('/low-stock-items')}>Display Low Stock Items</button>
+      <div className="dashboard-content">
+        <h1>Inventory Management Dashboard</h1>
+        <div className="buttons-container">
+          <button onClick={() => navigate('/add-item')} className="dashboard-button">Add Item</button>
+          <button onClick={() => navigate('/update-item')} className="dashboard-button">Update Item</button>
+          <button onClick={() => navigate('/remove-item')} className="dashboard-button">Remove Item</button>
+          <button onClick={() => navigate('/display-items-category')} className="dashboard-button">Display Items by Category</button>
+          <button onClick={() => navigate('/display-all-items')} className="dashboard-button">Display All Items</button>
+          <button onClick={() => navigate('/search-item')} className="dashboard-button">Search Item</button>
+          <button onClick={() => navigate('/sort-items')} className="dashboard-button">Sort Items</button>
+          <button onClick={() => navigate('/low-stock-items')} className="dashboard-button">Display Low Stock Items</button>
+        </div>
       </div>
     </div>
   );
