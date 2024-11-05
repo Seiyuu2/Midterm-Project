fba8532174caad2280dbe862af00c717fb3e1c83
Seiyuu2 <kurtlylep@gmail.com>
2024-11-05 21:05:28 +0800

    Testing process

--- midterm-project/src/components/Dashboard.js
+++ midterm-project/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 // ID duplicate check// ID duplicate check// ID duplicate check
+// ID duplicate check// ID duplicate check// ID duplicate check
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './styles/DashboardStyles.css';
